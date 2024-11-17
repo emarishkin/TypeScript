64af14906968c32860b30b468ebb86953145c24d
eemarishkin <ee.marishkin@yandex.ru>
2024-11-17 14:51:49 +0400

    TS-3-enum

--- 1_types.ts
+++ 1_types.ts
@@ -1,55 +1,55 @@
-// // const str: string = 'Hello'
-// // console.log(str)
+// const str: string = 'Hello'
+// console.log(str)
 
-// const isFetchinng: boolean = true
-// const int: number = 30
-// const float:number=30.2
-// const massage:string='hello world'
+const isFetchinng: boolean = true
+const int: number = 30
+const float:number=30.2
+const massage:string='hello world'
 
-// // массивы
+// массивы
 
-// const numberArray:number[] = [1,1,2,3,5,5,7,7]
-// const numberArray2:Array<number> = [1,1,2,3,5,5,7]
+const numberArray:number[] = [1,1,2,3,5,5,7,7]
+const numberArray2:Array<number> = [1,1,2,3,5,5,7]
 
-// const words:string[]=['hello','TypeScript']
+const words:string[]=['hello','TypeScript']
 
-// // tuple
-// const contact:[string,number] = ['Egor',112123]
+// tuple
+const contact:[string,number] = ['Egor',112123]
 
-// //any модкм измкнит переменную вне зависимости от ее типа
-// let variable:any=42
-// variable='new string'
-// variable=[]
+//any модкм измкнит переменную вне зависимости от ее типа
+let variable:any=42
+variable='new string'
+variable=[]
 
-// // =====
+// =====
 
-// function sayMyName(name:string):void{
-//     console.log(name)
-// }
-// sayMyName('egor')
+function sayMyName(name:string):void{
+    console.log(name)
+}
+sayMyName('egor')
 
-// // never
+// never
 
-// function throwError(massage:string): never{
-//     throw new Error(massage)
-// }
+function throwError(massage:string): never{
+    throw new Error(massage)
+}
 
-// function infinite():never{
-//     while(true){
+function infinite():never{
+    while(true){
 
-//     }
-// }
-//  // type создан к типа
+    }
+}
+ // type создан к типа
 
-// type Login = string
-// const login:Login='admin'
+type Login = string
+const login:Login='admin'
 
-// type ID= string | Number
-// const id1:ID=1
-// const id2:ID='123'
+type ID= string | Number
+const id1:ID=1
+const id2:ID='123'
 
-// // null & underfined
+// null & underfined
 
-// type SomeType = string | null | undefined
+type SomeType = string | null | undefined
 
 
---
+++ 3_enum.ts
@@ -0,0 +1,14 @@
+enum Membership {
+    Simple,
+    Standart,
+    Premium
+}
+const membership = Membership.Standart//1
+const membershipReverse =Membership[2]//Premium
+
+enum SocialMedia{
+  vk='vk',
+  Facebook='Facebook',
+  instagram='instagram'
+}
+const Social=SocialMedia.instagram//instagram
