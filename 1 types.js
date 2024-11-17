878a86c12badde655e46b200c1271fa8461cd54f
eemarishkin <ee.marishkin@yandex.ru>
2024-11-17 14:20:28 +0400

    TS-1

---
+++ 1 types.js
@@ -0,0 +1,21 @@
+// const str: string = 'Hello'
+// console.log(str)
+// var isFetchinng = true;
+// var int = 30;
+// var float = 30.2;
+// var massage = 'hello world';
+// // массивы
+// var numberArray = [1, 1, 2, 3, 5, 5, 7, 7];
+// var numberArray2 = [1, 1, 2, 3, 5, 5, 7];
+// var words = ['hello', 'TypeScript'];
+// // tuple
+// var contact = ['Egor', 112123];
+// //any модкм измкнит переменную вне зависимости от ее типа
+// var variable = 42;
+// variable = 'new string';
+// variable = [];
+// =====
+// function sayMyName(name) {
+//     console.log(name);
+// }
+// sayMyName('egor');
